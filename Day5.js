2b10ec36658aa14d9ac7b986a2b71e6a0e6d7b66
Krish-Anto <127953560+Krish-Anto@users.noreply.github.com>
2023-12-10 19:29:59 +0530

    Update and rename Day5 to Day5.js

--- Day5.js
+++ Day5.js
@@ -1,3 +1,4 @@
+
 //a) Write a constructor for the class Movie, which takes a 
 String representing the title of the movie, a String representing the studio, and a String representing the rating as its arguments, and sets the respective class properties to these values.
 
